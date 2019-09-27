d30587668d5256aa0a37517cda7046375f974fc1
Michiel Swaanen <32015956+michielswaanen@users.noreply.github.com>
2019-09-27 20:16:44 +0200

    Removed github spacing

--- src/be/uhasselt/student/Main.java
+++ src/be/uhasselt/student/Main.java
@@ -9,33 +9,33 @@ import be.uhasselt.student.visitor.FileExportVisitor;
  * @author Michiel Swaanen
  */
 public class Main {
-
+	
     public static void main(String[] args) {
-	    TreeNode root = new TreeNode("Bob");
+	TreeNode root = new TreeNode("Bob");
 
-	    TreeNode child1 = new TreeNode("Bob - Child 1");
-	    TreeNode child2 = new TreeNode("Bob - Child 2");
-	    TreeNode child3 = new TreeNode("Bob - Child 3");
+	TreeNode child1 = new TreeNode("Bob - Child 1");
+	TreeNode child2 = new TreeNode("Bob - Child 2");
+	TreeNode child3 = new TreeNode("Bob - Child 3");
 
-	    root.addChildren(child1, child2, child3);
+	root.addChildren(child1, child2, child3);
 
-	    //-----------------------------------
+	//-----------------------------------
 
-	    TreeNode child1Child1 = new TreeNode("Bob - Child 1 - Child 1");
-	    TreeNode child1Child2 = new TreeNode("Bob - Child 1 - Child 2");
+	TreeNode child1Child1 = new TreeNode("Bob - Child 1 - Child 1");
+	TreeNode child1Child2 = new TreeNode("Bob - Child 1 - Child 2");
 
-	    TreeNode child3Child1 = new TreeNode("Bob - Child 3 - Child 1");
+	TreeNode child3Child1 = new TreeNode("Bob - Child 3 - Child 1");
 
-		child1.addChildren(child1Child1, child1Child2);
-		child3.addChildren(child3Child1);
+	child1.addChildren(child1Child1, child1Child2);
+	child3.addChildren(child3Child1);
 
-		//-----------------------------------
+	//-----------------------------------
 
-		TreeNode childChildChild1 = new TreeNode("Bob - Child 1 - Child 1 - Child 1");
+	TreeNode childChildChild1 = new TreeNode("Bob - Child 1 - Child 1 - Child 1");
 
-		child1Child1.addChildren(childChildChild1);
+	child1Child1.addChildren(childChildChild1);
 
-		//-----------------------------------
+	//-----------------------------------
 
         /** Visitors **/
         ConsoleExportVisitor console = new ConsoleExportVisitor();
